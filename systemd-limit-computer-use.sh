18030e3d63f19ff47d5ded15a79684c5d44f00e9
Lauri Jakku <lja@iki.fi>
2020-06-17 22:37:40 +0300

    Made humanreadable dates to message.

--- systemd-limit-computer-use.sh
+++ systemd-limit-computer-use.sh
@@ -5,7 +5,7 @@ SYSTEM_SHUTDOWN_AT="23:00"
 SYSTEM_USED_ALLOW_FROM="05:00"
 
 if [ -e $config ]; then
-	source $config
+	source config
 fi
 
 TS_SAVE="/tmp/systemd-computer-start-time.config"
@@ -13,7 +13,7 @@ TS_SAVE="/tmp/systemd-computer-start-time.config"
 TS_SHUTDOWN_AT=$(date +"%s" -d "$SYSTEM_SHUTDOWN_AT today")
 TS_SYSTEM_USED_ALLOW_FROM=$(date +"%s" -d "$SYSTEM_USED_ALLOW_FROM next day")
 
-if [ -e $TS_SAVE ];then
+if [ -e  ];then
 	source ${TS_SAVE}
 else
 	echo "TS_SHUTDOWN_AT=${TS_SHUTDOWN_AT}" > ${TS_SAVE}
@@ -22,14 +22,23 @@ fi
 
 TS_NOW=$(date +"%s")
 
+func_get_str_date_from_epoch()
+{
+	date +"%H:%M %d.%m.%Y" -d "@$1"
+}
+
+DATE_TS_TODAY=$(func_get_str_date_from_epoch($TS_SHUTDOWN_AT))
+DATE_TS_NEXTDAY=$(func_get_str_date_from_epoch($TS_SYSTEM_USED_ALLOW_FROM))
 
 if [ $TS_NOW -gt $TS_SHUTDOWN_AT -a $TS_NOW -lt $TS_SYSTEM_USED_ALLOW_FROM ]
 then
-msg="shutdown at $SYSTEM_SHUTDOWN_AT today and computer is not usable until $SYSTEM_USED_ALLOW_FROM next day"
+msg="shutdown at $DATE_TS_TODAY today and computer is not usable until $DATE_TS_NEXTDAY next day"
 
 	notify-send "Shutting down in $TIMEOUT"  "$msg"
 	logger -s -t "$msg"
 	shutdown -Pf now "$msg"
+else
+	logger -s -t "use of computer" "Computer use allowed."
 fi
 
 #fail tp restart by systemd
