d8ec10b30186d91f77175b3d73d94fd0de785412
Lauri Jakku <lja@iki.fi>
2020-06-17 22:28:35 +0300

    fixed a bug

--- systemd-limit-computer-use.sh
+++ systemd-limit-computer-use.sh
@@ -8,9 +8,20 @@ if [ -e $config ]; then
 	source config
 fi
 
-TS_NOW=$(date +"%s")
+TS_SAVE="/tmp/systemd-computer-start-time.config"
+
 TS_SHUTDOWN_AT=$(date +"%s" -d "$SYSTEM_SHUTDOWN_AT today")
 TS_SYSTEM_USED_ALLOW_FROM=$(date +"%s" -d "$SYSTEM_USED_ALLOW_FROM next day")
+
+if [ -e  ];then
+	source ${TS_SAVE}
+else
+	echo "TS_SHUTDOWN_AT=${TS_SHUTDOWN_AT}" > ${TS_SAVE}
+	echo "TS_SYSTEM_USED_ALLOW_FROM=${TS_SYSTEM_USED_ALLOW_FROM}" >> ${TS_SAVE}
+fi
+
+TS_NOW=$(date +"%s")
+
 
 if [ $TS_NOW -gt $TS_SHUTDOWN_AT -a $TS_NOW -lt $TS_SYSTEM_USED_ALLOW_FROM ]
 then
@@ -25,4 +36,3 @@ fi
 
 #fail tp restart by systemd
 exit 1
-
