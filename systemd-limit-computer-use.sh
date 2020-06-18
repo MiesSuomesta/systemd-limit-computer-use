b8a6eb3e792f6aa115aee8530ba7d37d72c9df4b
Lauri Jakku <lja@iki.fi>
2020-06-18 03:30:44 +0300

    time calc bug

--- systemd-limit-computer-use.sh
+++ systemd-limit-computer-use.sh
@@ -1,48 +1,73 @@
 #!/bin/sh
+
+func_get_str_date_from_epoch() {
+	date +"%H:%M %d.%m.%Y" -d "@$1"
+}
+
+
 config=/etc/default/computer-use-disabled-hours.config
 
 SYSTEM_SHUTDOWN_AT="23:00"
-SYSTEM_USED_ALLOW_FROM="05:00"
-TIMEOUT_SECS=15
-TIMEOUT_MS=$((${TIMEOUT_SECS}*1000))
+SYSTEM_USED_ALLOW_FROM="03:00"
+TIMEOUT_MINUTES=30
+TIMEOUT_MS=$(( ${TIMEOUT_MINUTES}*60*1000 ))
+TIMEOUT_SEC=$(( ${TIMEOUT_MINUTES}*60 ))
 
 if [ -e $config ]
 then
 	source $config
 fi
 
-TS_SAVE="/tmp/systemd-computer-start-time.config"
+# tmp file
+TS_SAVE="/usr/local/tmp/systemd-computer-start-time.config"
 
 TS_SHUTDOWN_AT=$(date +"%s" -d "$SYSTEM_SHUTDOWN_AT today")
-TS_SYSTEM_USED_ALLOW_FROM=$(date +"%s" -d "$SYSTEM_USED_ALLOW_FROM next day")
 
-if [ -e  ];then
+TS_SYSTEM_USED_ALLOW_FROM_TD=$(date +"%s" -d "$SYSTEM_USED_ALLOW_FROM today")
+TS_SYSTEM_USED_ALLOW_FROM_ND=$(date +"%s" -d "$SYSTEM_USED_ALLOW_FROM next day")
+
+if [ -e ${TS_SAVE}  ];then
 	source ${TS_SAVE}
 else
 	echo "TS_SHUTDOWN_AT=${TS_SHUTDOWN_AT}" > ${TS_SAVE}
-	echo "TS_SYSTEM_USED_ALLOW_FROM=${TS_SYSTEM_USED_ALLOW_FROM}" >> ${TS_SAVE}
+	echo "TS_SYSTEM_USED_ALLOW_FROM_TD=${TS_SYSTEM_USED_ALLOW_FROM_TD}" >> ${TS_SAVE}
+	echo "TS_SYSTEM_USED_ALLOW_FROM_ND=${TS_SYSTEM_USED_ALLOW_FROM_ND}" >> ${TS_SAVE}
 fi
 
 TS_NOW=$(date +"%s")
 
-func_get_str_date_from_epoch() {
-	date +"%H:%M %d.%m.%Y" -d "@$1"
-}
-
 DATE_TS_TODAY=$(func_get_str_date_from_epoch $TS_SHUTDOWN_AT)
-DATE_TS_NEXTDAY=$(func_get_str_date_from_epoch $TS_SYSTEM_USED_ALLOW_FROM)
 
-if [ $TS_NOW -gt $TS_SHUTDOWN_AT -a $TS_NOW -lt $TS_SYSTEM_USED_ALLOW_FROM ]
+YEA=0
+if [ $TS_NOW -gt $TS_SHUTDOWN_AT ]
 then
-	msg="shutdown at $DATE_TS_TODAY today and computer is not usable until $DATE_TS_NEXTDAY next day"
 
-	notify-send -t $TIMEOUT_MS "Shutting down in $TIMEOUT !!"  "$msg"
-	logger -s -t "$msg"
-	shutdown -Pf +$TIMEOUT "$msg"
-	while true
-	do
-		sleep 10
-	done
+	if [ $TS_NOW -lt $TS_SYSTEM_USED_ALLOW_FROM_TD ]
+	then
+		YEA=1
+		DATE_TS_NEXTDAY=$(func_get_str_date_from_epoch $TS_SYSTEM_USED_ALLOW_FROM_TD)
+
+	else
+		if [ $TS_NOW -lt $TS_SYSTEM_USED_ALLOW_FROM_ND ]
+		then
+			YEA=2
+			DATE_TS_NEXTDAY=$(func_get_str_date_from_epoch $TS_SYSTEM_USED_ALLOW_FROM_ND)
+		fi
+	fi
+fi
+
+
+
+if [ $YEA -gt 0 ]
+then
+	TO=$((${TS_NOW} + ${TIMEOUT_SEC}))
+	DATE_TS_SD=$(func_get_str_date_from_epoch $TO)
+	msg="Shutdown at ${DATE_TS_SD}. Computer is not nusable until $DATE_TS_NEXTDAY next day"
+
+	notify-send -a "Sleep rythm enforcer" -t $TIMEOUT_MS "Shutting down in ${TIMEOUT_MINUTES} minutes."  "$msg"
+	logger -t "Sleep rythm enforcer" "$msg"
+	rm $TS_SAVE
+	shutdown -Pf "+$TIMEOUT_MINUTES" "$msg"
 fi
 
 #fail tp restart by systemd
